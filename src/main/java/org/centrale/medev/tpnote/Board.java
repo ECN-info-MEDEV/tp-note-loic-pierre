25bacbf4dcd06a2613cc0878779f0b869a062515
LocPat <loic.patigny@yahoo.fr>
2023-01-26 08:39:02 +0100

    Initiation of Board Class

---
+++ src/main/java/org/centrale/medev/tpnote/Board.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.medev.tpnote;
+
+/**
+ * Classe décrivant le plateau de jeu
+ * @author loicpatigny
+ */
+public class Board {
+    
+}
