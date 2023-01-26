1c760ec7c52f4178ab673a3416885e77d38ea0e9
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-01-26 09:31:45 +0100

    Create a main class

---
+++ src/main/java/org/centrale/medev/tpnote/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.medev.tpnote;
+
+/**
+ * Main class
+ * @author Pierre Baudet, Loïc Patigny
+ */
+public class Main {
+    
+    /**
+     * Main method
+     * @param args the arguments of the main method
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
