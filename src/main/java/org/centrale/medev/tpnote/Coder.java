8ab363a202cfa90ab587280016526d8c33e46a06
Pierre <72155300+Pierre36@users.noreply.github.com>
2023-01-26 10:16:13 +0100

    Handle the black and white pieces

--- src/main/java/org/centrale/medev/tpnote/Coder.java
+++ src/main/java/org/centrale/medev/tpnote/Coder.java
@@ -5,6 +5,7 @@
 package org.centrale.medev.tpnote;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -32,6 +33,46 @@ public class Coder extends Player {
     public List<Integer> getCode() {
         System.out.println("Create a secret code.");
         return super.getCombination();
+    }
+    
+    
+    // Evaluate the guess
+    
+    /**
+     * Use a scanner to get the number of pieces.
+     * @return the number of pieces
+     */
+    private int getNbPieces() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isInteger = false;
+        int nbPieces = 0;
+        while (!isInteger) {
+            try {
+                nbPieces = scanner.nextInt();
+                isInteger = true;
+            } catch (NoSuchElementException e) {
+                isInteger = false;
+            }
+        }
+        return nbPieces;
+    }
+    
+    /**
+     * Get the number of white pieces the coder wants to add.
+     * @return the number of white pieces
+     */
+    public int getNbWhite() {
+        System.out.println("How many white pieces do you want to indicate?");
+        return getNbPieces();    
+    }
+    
+    /**
+     * Get the number of black pieces the coder wants to add.
+     * @return the number of black pieces
+     */
+    public int getNbBlack() {
+        System.out.println("How many black pieces do you want to indicate?");
+        return getNbPieces();    
     }
      
 }
